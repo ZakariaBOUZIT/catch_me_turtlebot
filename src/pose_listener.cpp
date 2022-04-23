aaf7c85a731dc19fdedfde3b4438418afbe17397
zackq <zakaria.elhabib.bouzit@gmail.com>
2022-04-24 01:30:36 +0200

    version 1.0

---
+++ src/pose_listener.cpp
@@ -0,0 +1,18 @@
+#include <ros/ros.h>
+#include <turtlesim/Pose.h>
+
+turtlesim::Pose turtle_pose;
+
+void pose_callback(const turtlesim::Pose::ConstPtr pose_msg){
+    ROS_INFO("listening to turtle pose :");
+    turtle_pose = *pose_msg; 
+    ROS_INFO_STREAM("x : " <<turtle_pose.x << "\ty : " <<turtle_pose.y << "\ttheta : " <<turtle_pose.theta);
+}
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "pose_listener");
+    ros::NodeHandle n;
+    ros::Subscriber sub = n.subscribe("turtle1/pose", 10, pose_callback);
+    ros::spin();
+    return 0;
+}
